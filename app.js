3cf2ca9656d7175e5a1cefe1943fa62db5fe4deb
Dhruva Maheshwari <maheshwaridhruva0@gmail.com>
2026-08-19 22:26:17 +0530

    understand the Basic type of the Ts

---
+++ app.js
@@ -0,0 +1,17 @@
+"use strict";
+// Basic Types
+Object.defineProperty(exports, "__esModule", { value: true });
+// Primitve types (number , string , boolean)
+let a = 12;
+// array
+let arr = [1, 2, 2, "name"];
+// tuples
+let tuple = ['dhruva', 4];
+// Enums
+var UserRole;
+(function (UserRole) {
+    UserRole["ADMIN"] = "admin";
+    UserRole["GUEST"] = "guest";
+    UserRole["SUPER_ADMIN"] = "super_admin";
+})(UserRole || (UserRole = {}));
+// any , unknow , void , NUll , undefinded , never
---
+++ app.ts
@@ -0,0 +1,40 @@
+/** @format */
+
+// Basic Types
+
+// Primitve types (number , string , boolean)
+let a = 12;
+
+// array
+let arr = [1, 2, 2, "name"];
+
+// tuples
+let tuple: [string, number] = ["dhruva", 4];
+
+// Enums
+enum UserRole {
+  ADMIN = "admin",
+  GUEST = "guest",
+  SUPER_ADMIN = "super_admin",
+}
+
+// any , unknown , void , NUll , undefinded , never
+let b: any; // define the type
+b = 3;
+b = "name";
+// simple any ka mtld type script off ho jati  h
+
+
+let c: unknown;
+c = 12;
+c = "dhruva";
+
+if (typeof c == "string") c.toUpperCase(); // this is called type narrowing
+
+
+
+function adcd():void{  // jab koi fun. bano ge to usa reutn type batana pde ga like (void , boolen , number)
+    console.log('this fun. does not return any thing')
+}
+
+
